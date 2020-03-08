941d048448097e9490a0aa4bc1fe856848c094db
polly-foz <polly.foz@gmail.com>
2020-03-09 01:22:07 +0800

    button style use parcel

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 实现一个简单的ui框架
+
+## 安装
+
+使用前，请在CSS设置border-box
+
+```css
+*{
+    box-sizing:border-box;
+}
+```
---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="pf-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: "Button"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .pf-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+        &:focus{
+            outline:none;
+        }
+    }
+
+</style>
